6d22499823a67e4f2651a6d043d8b0a67130cdb7
vatsuvaksi <62776100+vatsuvaksi@users.noreply.github.com>
2021-03-19 01:41:58 +0530

    Added book routes

--- back-end/routes/bookRoute.js
+++ back-end/routes/bookRoute.js
@@ -2,6 +2,11 @@ const express = require('express');
 const Book  = require('../models/Book');
 const bookRoute = express.Router();
 
+
+//            <------------Routes---------------->
+//------Books Routes-------
+
+
 //Create Books
 bookRoute.post('/', async function(req,res){
     const book = await Book.create(req.body);
@@ -16,8 +21,9 @@ bookRoute.post('/', async function(req,res){
 });
 
 
+//find books
 bookRoute.get('/', async function(req,res){
-    const book = await Book.find({});
+    const book =  await Book.find().populate('createdBy').sort('createdAt');
 
     if(book){
         res.status(200);
@@ -27,6 +33,52 @@ bookRoute.get('/', async function(req,res){
         res.send("Creating fail");
     }
 });
+
+
+//Delete books by id 
+bookRoute.delete('/:id', async function(req,res){
+    const book = await Book.findByIdAndDelete(req.params.id);
+        res.status(200);
+        res.json(book);
+  
+        res.status(500);
+        res.send("Creating fail");
+    
+});
+
+//Update books by id 
+bookRoute.put(
+    '/:id',
+  async function (req, res) {
+      const book = await Book.findById(req.params.id);
+
+      if(book){
+       const updatedBook = await Book.findByIdAndUpdate(
+           req.params.id,
+           req.body,{
+               new :true,
+               runValidators : true
+           }
+       )
+      }else{
+          throw new error("Not working"); 
+      }
+    });
+
+//find a book by Id 
+bookRoute.get(
+    '/:id',
+     (async (req, res) => {
+      try {
+        const book = await Book.findById(req.params.id);
+        res.status(200);
+        res.send(book);
+      } catch (error) {
+        res.status(500);
+        throw new Error('No book found');
+      }
+    })
+  );
 
 
 module.exports = bookRoute;
